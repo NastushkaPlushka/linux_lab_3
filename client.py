a8011c3cbb0b160b620504ce11f3e865455ccf15
Anastasia <Anastasia_20_00@mail.ru>
2021-04-08 15:58:07 +0300

    commit

---
+++ client.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-import socket
+import socket
+from time import sleep
+
+
+sock = socket.socket()
+address = input("Введите адрес для подключения: ")
+if not address or address == '':
+    address = "localhost"
+
+port = input("Введите номер порта: ")
+if not port or port == '':
+    port = 9090
+sock.connect(('localhost', 9090))
+
+connection = True
+
+send = input()
+msg = send
+print("Началась отправка данных серверу!")
+sock.send("user1".encode())  
+
+
+while True:
+
+    send = input()
+    msg = send
+    print("Началась отправка данных серверу!")
+    sock.send(msg.encode())
+
+    print("Прием данных от сервера!")
+    data = sock.recv(1024)
+    print(data)
+    print(data.decode())
